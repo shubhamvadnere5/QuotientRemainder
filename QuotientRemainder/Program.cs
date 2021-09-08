3f60b082e0eda4bdad4510ac2283d06dcad005c9
ShubhamPV <shubhamvadnere5@gmail.com>
2021-09-08 09:16:24 +0530

    Adding Q&R Project

---
+++ QuotientRemainder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Dividend = 20, Divisor= 5 ;
+            int Quotient = Dividend / Divisor;
+            int Remainder = Dividend % Divisor;
+            Console.WriteLine("Dividend:{0} Divisor:{1}", Dividend, Divisor);
+            Console.WriteLine("quotient=" + Quotient);
+            Console.WriteLine("Remainder=" + Remainder);
+            Console.ReadLine();
+        }
+    }
+}
